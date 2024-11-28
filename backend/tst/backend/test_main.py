3df648bd086f06197a8aa72f5694f429789895ba
Dev Rishi Khare <devrishik@gmail.com>
2024-11-28 15:21:31 -0500

    initial backend commit

--- backend/tst/backend/test_main.py
+++ backend/tst/backend/test_main.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 
-from main import app
+from backend.main import app
 
 client = TestClient(app)
 
@@ -8,4 +8,5 @@ client = TestClient(app)
 def test_default():
     response = client.get("/")
     assert response.status_code == 200
-    assert "response" in response.json()
+    assert "message" in response.json()
+    assert response.json()["message"] == "Hello World"
